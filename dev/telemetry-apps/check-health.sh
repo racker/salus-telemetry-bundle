f616d37196d397ef9ce7de38fd6ac967d285c9f2
Geoff Bourne <itzgeoff@gmail.com>
2019-06-04 16:33:47 -0500

    Adding presence-monitor

--- dev/telemetry-apps/check-health.sh
+++ dev/telemetry-apps/check-health.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-for p in 8080 8081 8082 8084 8085 8087 8088 8089 8888; do
+for p in 8080 8081 8082 8083 8084 8085 8087 8088 8089 8888; do
   echo $p
   curl localhost:$p/actuator/health
   echo
